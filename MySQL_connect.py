0c850cf803df598e35fcb2cc21ea16ca7b7d87b3
emmet1991 <emmetgngls5@gmail.com>
2018-10-21 21:58:12 +0100

    added connections file

---
+++ MySQL_connect.py
@@ -0,0 +1,7 @@
+# This file is used for providing the connection parameters to MySQL. Please input the relevant values below
+config = {
+	'user': '',
+	'password': '',
+	'host': 'localhost',
+	'db': ''
+}
